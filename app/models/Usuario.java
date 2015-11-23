bb8a0c54bec80eca8fdbe0aaf7977a9f676c0a71
RoddrigoRamos <rodrigoramosnajera@gmail.com>
2015-11-23 16:10:45 -0700

    Heroku.

--- app/models/Usuario.java
+++ app/models/Usuario.java
@@ -1,10 +1,10 @@
 
 package models;
 
-import org.mongodb.morphia.annotations.Entity;
 import java.util.Date;
 import play.data.validation.*;
 import play.modules.morphia.Model;
+import org.mongodb.morphia.annotations.Entity; 
 
 /**
  *
