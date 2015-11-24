64a346248865efd4ee0b6418e7df44760ba1a130
AlejandroRdgz <a271578@uach.mx>
2015-11-23 19:44:20 -0700

    wSS.

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -13,7 +13,7 @@ public class BasicTest extends UnitTest {
     @Test
     public void crearYRecuperarUsuario(){
         //Crear nuevo usuario y guardarlo
-        new Usuario("Alex", "alex@gmail.com", "123456").save();
+//        new Usuario("Alex", "alex@gmail.com", "123456").save();
         
         //Recuperar el usuario con la direccion de email alex@gmail.com
         Usuario alex = Usuario.find("byEmail","alex@gmail.com").first();
